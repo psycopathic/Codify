b3b2c096a601efef11a98b25f3833da9ca44bc7a
psycopathic <harshsrivastava2502@gmail.com>
2025-09-16 23:22:23 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import prisma from '@/lib/db'
+"use client";
+import React from "react";
+import { useTRPC } from "@/trpc/client"; //useTRPC is a custom hook to use tRPC in React components
+import { useQuery } from "@tanstack/react-query";
 
-const page = async() => {
-  // const users = await prisma.user.findMany();
+const page = () => {
+  const trpc = useTRPC();
+  const {data} = useQuery(trpc.createAI.queryOptions({text:"Harsh"}));
+  
+  
   return (
     <div>
-      <h1>HI I am harsh shrivatava</h1>
+      <h1>{JSON.stringify(data)}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
